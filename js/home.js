9f217a66a7aeeb1e62bc9d21cde6120ac9317408
Hemangkhedikar <hemangkhedikar@3gmail.com>
2026-01-19 18:49:16 +0530

    Some js added in Home page

--- js/home.js
+++ js/home.js
@@ -1,3 +1,34 @@
+const data = JSON.parse(localStorage.getItem("profileData"));
+
+if (data) {
+
+  // Name
+  document.getElementById("sidebarName").innerText =
+    data.firstName + " " + data.lastName;
+
+  // Education
+  document.getElementById("sidebarEducation").innerText =
+    data.education || "Student";
+
+  // Location
+  document.getElementById("sidebarLocation").innerText =
+    data.city || "India";
+
+  // Profile Pic
+  if (data.profileImage) {
+    document.getElementById("sidebarPic").src = data.profileImage;
+  }
+
+  // Cover Pic
+  if (data.coverImage) {
+    document.getElementById("sidebarCover").src = data.coverImage;
+  }
+  // Middle pic
+  if (data.profileImage) {
+  document.getElementById("middleProfilePic").src = data.profileImage;
+}
+
+}
 
 
 let sideActivity = document.getElementById("recentActivities");
